3c2795977e67601be3c32f772a90a7d6438d2840
mahin <mahin@DESKTOP-L5PVBBB>
2025-03-13 22:14:39 +0800

    SOUND AND NEW ITEMS

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -25,17 +25,19 @@ public class GamePanel extends JPanel implements Runnable {
     // World Settings
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxWorldRow;
+ 
     
     // FPS 
     int FPS = 60;
     
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
-    Thread gameThread;
+    Sound sound = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public Player player = new Player(this,keyH);
+    Thread gameThread;
+    
+    //ENTITY AND OBJECTS
     public SuperObject obj[] = new SuperObject[10];
     public AssetSetter aSetter = new AssetSetter(this);
 
@@ -53,6 +55,8 @@ public class GamePanel extends JPanel implements Runnable {
     
     public void setupGame() {
     	aSetter.setObject();
+    	
+    	playMusic(0);
     }
 
     public void startGameThread() {
@@ -112,5 +116,20 @@ public class GamePanel extends JPanel implements Runnable {
         player.draw(g2);
         
         g2.dispose();
+    }
+    
+    public void playMusic(int i) {
+    	
+    	sound.setFile(i);
+    	sound.play();
+    	sound.loop();
+    }
+    public void stopMusic() {
+    	sound.stop();
+    }
+    
+    public void playSE (int i) {
+    	sound.setFile(i);
+    	sound.play();
     }
 }
